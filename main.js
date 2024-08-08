6a91a84c45b1c8ecf4ea4d0e34e61164a8e766c6
Daniel Cózar Grafiá <dcozargrafia@gmail.com>
2024-08-08 21:22:21 +0200

    Añadido el icono en la ventana de aplicación y cambiado el tamaño inicial

--- main.js
+++ main.js
@@ -5,7 +5,8 @@ const path = require('path');
 function createWindow () {
   const win = new BrowserWindow({
     width: 800,
-    height: 600,
+    height: 800,
+    icon: path.join(__dirname, 'assets/icon.png'),
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false
